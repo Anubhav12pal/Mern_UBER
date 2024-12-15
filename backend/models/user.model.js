dbc9b26e1b2ad3b0ca98c407680c3d5885ccf5be
Anubhav Pal <anubhavpal97@gmail.com>
2024-12-14 19:20:04 -0600

    Fixing the schema and user validation

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,44 +3,46 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-    minlength: [3, "First name must be atleast 3 characters long"],
-  },
-  lastname: {
-    type: String,
-
-    required: true,
-    minlength: [2, "Last name must be atleast 2 characters long"],
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "First name must be at least 3 characters long"],
+    },
+    lastname: {
+      type: String,
+      required: true,
+      minlength: [2, "Last name must be at least 2 characters long"],
+    },
   },
   email: {
     type: String,
     required: true,
-
     unique: true,
-    minlength: [5, "Email must be atleast 5 characters long"],
+    minlength: [5, "Email must be at least 5 characters long"],
   },
-  passord: {
+  password: {
     type: String,
     required: true,
-    minlength: [8, "Password must be atleast 8 characters long"],
+    minlength: [8, "Password must be at least 8 characters long"],
   },
-
   socketId: {
     type: String,
   },
 });
 
+// Instance method to generate auth token
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
   return token;
 };
 
+// Instance method to compare passwords
 userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+// Static method to hash password
 userSchema.statics.hashPassword = async function (password) {
   return await bcrypt.hash(password, 10);
 };
